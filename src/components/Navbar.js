7cd357dfe2b984331c4a531f11f1150c34445f52
Blucilha <blucilha.nascimento@gmail.com>
2021-10-17 02:11:58 -0300

    Insere icon na barra lateral

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,12 @@
 import React from "react";
 import './styles/Navbar.css';
+import { AiOutlineSetting } from 'react-icons/ai';
 
 function Navbar(){
 	return (
 		<aside className='bar-lateral'>
 			<nav id='nav-bar'>
-				<p>Administração</p>
+				<p><AiOutlineSetting /> Administração</p>
 				<a className='link' href='http://localhost:3000/'>Administradores</a>
 				<a className='link' href='http://localhost:3000/'>Áreas</a>
 				<a className='link' href='http://localhost:3000/'>Locais de Trabalho</a>
